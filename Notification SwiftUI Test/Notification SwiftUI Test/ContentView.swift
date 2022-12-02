1a8930956938392060dae0d14e7d30b3625ea27e
Michael Bonfanti <53140578+MichaelBonfanti@users.noreply.github.com>
2022-12-02 09:47:58 +0100

    Add Example Code

---
+++ Notification SwiftUI Test/Notification SwiftUI Test/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Notification SwiftUI Test
+//
+//  Created by Michael Bonfanti on 02/12/22.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }            }
+
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Notification Test"
+                content.subtitle = "This notification is only a test notification"
+                content.sound = UNNotificationSound.default
+
+                // show this notification five seconds from now
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                // choose a random identifier
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                // add our notification request
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Notification SwiftUI Test/Notification SwiftUI Test/Notification_SwiftUI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notification_SwiftUI_TestApp.swift
+//  Notification SwiftUI Test
+//
+//  Created by Michael Bonfanti on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Notification_SwiftUI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
